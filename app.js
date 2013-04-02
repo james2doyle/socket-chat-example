00020a6cadf19b8c6f8829e15348c6022f622538
james2doyle <james2doyle@gmail.com>
2013-04-02 16:16:04 -0400

    add a bunch of comments

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-type': 'text/html'
   });
+  // read the index.html file
   res.end(fs.readFileSync(__dirname + '/index.html'));
 }).listen(8080, function() {
   console.log('Listening at: http://localhost:8080');
 });
 
+// declare the global var. this is volatile
 var count = 0;
 
+// create the server connnection
 socketio.listen(server).on('connection', function(socket) {
+  // message is a reserved event
   socket.on('message', function(msg) {
+    // show it in the node console
     console.log('Message Received: ', msg);
+    // emit it to the other clients
     socket.broadcast.emit('message', msg);
   });
   // define the event
